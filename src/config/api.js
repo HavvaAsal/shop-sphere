720e2069aac485264469cd1c899532ec322528d5
HavvaAsal <havvasal20@gmail.com>
2024-12-17 13:01:58 +0300

    api.js ekler

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = "https://workintech-fe-ecommerce.onrender.com";
+
+export const ENDPOINTS = {
+    LOGIN: `${API_BASE_URL}/login`,
+    SIGNUP: `${API_BASE_URL}/signup`,
+    // Diğer endpoint'leri buraya ekleyebilirsiniz
+};
