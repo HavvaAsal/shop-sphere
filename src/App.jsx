1c45f8ce7ad7300bbb05f9d6c967ea8aac231138
HavvaAsal <havvasal20@gmail.com>
2024-12-17 22:30:25 +0300

    router ekler

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./index.css";
 import Header from "./layout/Header";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-white">
-        <ToastContainer position="top-right" autoClose={3000} />
+    <Router>
+      <>
         <Header />
-        <HomePage />
-      </div>
-    </BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </>
+    </Router>
   );
 }
 
