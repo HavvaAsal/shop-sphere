c20cab1e6dd998488eaf51ffe48bfd05c76a25b2
HavvaAsal <havvasal20@gmail.com>
2024-12-19 16:40:07 +0300

    shadcn kurar

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
