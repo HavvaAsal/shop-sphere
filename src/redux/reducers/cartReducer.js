3389bafa6b61ce79184ff32a81cf7538e517988a
HavvaAsal <havvasal20@gmail.com>
2024-12-27 13:43:30 +0300

    Generates cart reducer

---
+++ src/redux/reducers/cartReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  items: [],
+  total: 0
+};
+
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // Cart işlemleri...
+    default:
+      return state;
+  }
+};
+
+export default cartReducer; 
