7e02aeca147381054f7167d5e9175e04c166b375
HavvaAsal <havvasal20@gmail.com>
2024-12-27 13:51:04 +0300

    react-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { StrictMode } from 'react';
 import store from './redux/store';
 import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </StrictMode>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
